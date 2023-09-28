44754532f0ffd385b3ac5fa14b926700dae81ca4
liminat <180078053+liminat@users.noreply.github.com>
2023-09-28 14:23:32 +0000

    fixed #28

---
+++ hlss/datasets/improc.py
@@ -0,0 +1,7 @@
+"""Image processing functions designed to work with OpenSRH and TCGA datasets.
+
+Copyright (c) 2024 Mohamed Bin Zayed University of Artificial Intelligence. All rights reserved.
+Licensed under the MIT License. See LICENSE for license information.
+"""
+
+from typing import Optional
