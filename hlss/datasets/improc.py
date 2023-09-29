b41a6e3fe3b603f750c1ace1269e788e049553af
liminat <180078053+liminat@users.noreply.github.com>
2023-09-29 06:53:22 +0000

    Removed log statement

--- hlss/datasets/improc.py
+++ hlss/datasets/improc.py
@@ -4,4 +4,30 @@ Copyright (c) 2024 Mohamed Bin Zayed University of Artificial Intelligence. All 
 Licensed under the MIT License. See LICENSE for license information.
 """
 
-from typing import Optional
+from typing import Optional, List, Tuple, Dict
+from functools import partial
+from PIL import Image
+import random
+import tifffile
+import numpy as np
+import h5py
+import torch
+from torch.nn import ModuleList
+from torchvision.transforms import (
+    Normalize, RandomApply, Compose, RandomHorizontalFlip, RandomVerticalFlip,
+    Resize, RandAugment, RandomErasing, RandomAutocontrast, Grayscale,
+    RandomSolarize, ColorJitter, RandomAdjustSharpness, GaussianBlur,
+    RandomAffine, RandomResizedCrop, CenterCrop)
+from torchvision.transforms import Compose, Resize,ToPILImage, CenterCrop, ToTensor, Normalize, RandomResizedCrop
+
+class GetThirdChannel(torch.nn.Module):
+    """Computes the third channel of SRH image
+
+    Compute the third channel of SRH images by subtracting CH3 and CH2. The
+    channel difference is added to the subtracted_base.
+
+    """
+
+    def __init__(self, subtracted_base: float = 5000 / 65536.0):
+        super().__init__()
+        self.subtract
